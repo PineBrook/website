5a875f165b156187183328e5a1081269ddd7eecf
yashvardhan <vs21yash@gmail.com>
2026-07-09 13:57:15 +0530

    add base application routing config

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+/**
+ * @license
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navbar } from "./components/Navbar";
+import { Footer } from "./components/Footer";
+import { Home } from "./pages/Home";
+import { Mission } from "./pages/Mission";
+
+export default function App() {
+  return (
+    <Router>
+      <div className="min-h-screen bg-brand-surface selection:bg-brand-primary/30 selection:text-white">
+        <Navbar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/mission" element={<Mission />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
