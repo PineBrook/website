0726a32bfd2dcbd1be1ca51a3722655bfe693070
yashvardhan <vs21yash@gmail.com>
2026-07-11 15:16:13 +0530

    feat: switch scheduling integration to Google Calendar booking link

--- src/hooks/useCal.ts
+++ src/hooks/useCal.ts
@@ -9,54 +9,20 @@ declare global {
 
 export function openCal(e?: React.MouseEvent<HTMLElement>) {
   if (typeof window !== "undefined") {
-    const target = e?.currentTarget;
-    const calLink = target?.getAttribute("data-cal-link") || "pinebrook";
-    if (window.CalIsLoaded && window.Cal) {
-      window.Cal("openModal", {
-        calLink,
-        config: { layout: "month_view" }
-      });
-    } else {
-      window.open(`https://cal.com/${calLink}`, "_blank");
-    }
+    const width = 800;
+    const height = 750;
+    const left = window.screen.width / 2 - width / 2;
+    const top = window.screen.height / 2 - height / 2;
+
+    window.open(
+      "https://calendar.app.google/7PdCsQddyDbsyNHt7",
+      "GoogleCalendar",
+      `width=${width},height=${height},top=${top},left=${left},resizable=yes,scrollbars=yes`
+    );
   }
 }
 
 export function useCalEmbed() {
-  useEffect(() => {
-    (function (C: any, A: any, L: string, t: string) {
-      C["Cal"] = C["Cal"] || function () {
-        let o = C["Cal"].q = C["Cal"].q || [];
-        o.push(arguments);
-      };
-      C["Cal"].loaded = true;
-      let p = A.createElement(L);
-      p.src = t;
-      p.async = true;
-      p.onload = () => {
-        C["CalIsLoaded"] = true;
-      };
-      let firstScript = A.getElementsByTagName(L)[0];
-      if (firstScript && firstScript.parentNode) {
-        firstScript.parentNode.insertBefore(p, firstScript);
-      } else {
-        A.head.appendChild(p);
-      }
-    })(window, document, "script", "https://embed.cal.com/embed/parent.js");
-
-    window.Cal?.("init", {
-      origin: "https://cal.com",
-    });
-
-    window.Cal?.("ui", {
-      theme: "dark",
-      styles: {
-        branding: {
-          brandColor: "#020617",
-        },
-      },
-      hideEventTypeDetails: false,
-      layout: "month_view",
-    });
-  }, []);
+  // No-op since we are using Google Calendar popup directly for 100% reliability
+  useEffect(() => {}, []);
 }
