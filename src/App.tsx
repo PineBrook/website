be39bc35172f3e00c03840b29485d45ab55a52a9
yashvardhan <vs21yash@gmail.com>
2026-07-14 17:51:24 +0530

    feat: add vercel analytics and speed insights

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import { Navbar } from "./components/Navbar";
 import { Footer } from "./components/Footer";
 import { Home } from "./pages/Home";
@@ -26,6 +27,7 @@ export default function App() {
         </main>
         <Footer />
         <Analytics />
+        <SpeedInsights />
       </div>
     </Router>
   );
